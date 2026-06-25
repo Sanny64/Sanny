89ea16e5c1a22214f24f0ed8dae43b07db7fb937
Sanny <santino.pia04@gmx.de>
2026-06-25 04:44:34 +0200

    Implement shared ui components feat: created component: Section feat: created component: Button feat: created component: ButtonGroup feat: created advanced component: SharedSetupProbe todo: finish page: Settings todo: finish page: Error

---
+++ shared/packages/ui/src/index.ts
@@ -0,0 +1,5 @@
+export { Button } from './components/Button';
+export { Section } from './components/Section';
+export { SharedSetupProbe } from './components/SharedSetupProbe';
+export { default as Error } from './pages/errors/Error';
+export { default as Settings } from './pages/settings/Settings';
