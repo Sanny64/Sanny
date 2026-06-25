5d79a16ac83c7dda0c5954ffe1125775951656a9
Sanny <santino.pia04@gmx.de>
2026-06-25 04:53:06 +0200

    Implement login scaffolding
    feat: added main pages and routing

--- login/src/main.tsx
+++ login/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { ThemeProvider } from '@sanny/styles'
+import { LanguageProvider } from '@sanny/i18n'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* Context Providers */}
+    <ThemeProvider>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
